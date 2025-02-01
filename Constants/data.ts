8d5c3e2e5b143cb262e78c8d55e5fcbeb65364aa
LeoU-98 <mo7amedmedo9898@gmail.com>
2025-02-01 23:31:29 +0200

    fast one

--- Constants/data.ts
+++ Constants/data.ts
@@ -1,42 +1,63 @@
-export const characterFilterData = [
-  {
-    name: "name",
-  },
-  {
-    name: "status",
-    options: ["alive", "dead", "unknown"],
-  },
-  {
-    name: "speices",
-  },
-  {
-    name: "type",
-  },
-  {
-    name: "gender",
-    options: ["female", "male", "genderless", "unknown"],
-  },
-];
+// export const characterFilterData = [
+//   {
+//     name: "status",
+//     options: ["Alive", "Dead", "Unknown"],
+//     value: null,
+//   },
+//   {
+//     name: "speices",
+//     options: ["Human", "Alien"],
+//     value: null,
+//   },
+//   {
+//     name: "gender",
+//     options: ["Female", "Male", "Genderless", "Unknown"],
+//     value: null,
+//   },
+// ];
 
-export const locationFilterData = [
-  {
-    name: "name",
-  },
-  {
-    name: "type",
-    options: ["alive", "dead", "unknown"],
-  },
-  {
-    name: "dimension",
-  },
-];
+// export const locationFilterData = [
+//   {
+//     name: "type",
+//     options: ["Plant", "Space Station", "unknown"],
+//     value: null,
+//   },
+//   {
+//     name: "dimension",
+//     options: ["Dimension C-137", "unknown"],
+//     value: null,
+//   },
+// ];
 
-export const episodeFilterData = [
-  {
-    name: "name",
-  },
-  {
-    name: "episode",
-    options: ["alive", "dead", "unknown"],
-  },
-];
+// export const episodeFilterData = [
+//   {
+//     name: "type",
+//     options: ["Plant", "Space Station", "unknown"],
+//     value: null,
+//   },
+//   {
+//     name: "dimension",
+//     options: ["Dimension C-137", "unknown"],
+//     value: null,
+//   },
+// ];
+
+export const filterData = {
+  characters: [
+    { name: "status", options: ["Alive", "Dead", "Unknown"], value: "" },
+    { name: "species", options: ["Human", "Alien"], value: "" },
+    {
+      name: "gender",
+      options: ["Female", "Male", "Genderless", "Unknown"],
+      value: "",
+    },
+  ],
+  locations: [
+    { name: "type", options: ["Plant", "Space Station", "Unknown"], value: "" },
+    { name: "dimension", options: ["Dimension C-137", "Unknown"], value: "" },
+  ],
+  episodes: [
+    { name: "type", options: ["Plant", "Space Station", "Unknown"], value: "" },
+    { name: "dimension", options: ["Dimension C-137", "Unknown"], value: "" },
+  ],
+};
--- src/app/Characters/page.tsx
+++ src/app/Characters/page.tsx
@@ -1,5 +1,30 @@
-function CharactersPage() {
-  return <div></div>;
-}
+// import { characterFilterData } from "../../../Constants/data";
+import { filterData } from "../../../Constants/data";
+// import FilterComponent from "../FilterComponent";
+import FilterFilter from "../FilterFilter";
+import Search from "../Search";
+import SearchResults from "../SearchResults";
 
-export default CharactersPage;
+export default async function CharactersPage() {
+  const res = await fetch("https://rickandmortyapi.com/api/character/");
+  const data = await res.json();
+
+  console.log(data);
+
+  return (
+    <div className="container mx-auto mt-2 flex h-[740px] overflow-hidden rounded-lg border-2 border-white bg-black/70">
+      <div className="basis-1/5 border-r-2 border-r-white">
+        {/* <FilterComponent
+          filterBy="character"
+          filterData={characterFilterData}
+        /> */}
+
+        <FilterFilter filters={filterData} category="characters" />
+      </div>
+      <div className="basis-4/5">
+        <Search />
+        <SearchResults data={data} />
+      </div>
+    </div>
+  );
+}
