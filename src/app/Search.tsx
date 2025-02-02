74c35f6a26c381bfd4cad13ec9282eb0ee3bdd95
Mohamed Hamdy <mo7amedmedo9889@gmail.com>
2025-02-02 11:10:33 +0200

    fast one ,,, Navbar and Search Some Changes

--- src/app/Search.tsx
+++ src/app/Search.tsx
@@ -1,21 +1,26 @@
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
 function Search() {
+  const [searchText, setSearchText] = useState();
+
   return (
-    <div className="flex items-center justify-center gap-16 border-b-2 border-b-white py-3">
-      <input type="text" className="block w-80 rounded-xl px-4 py-2" />
-      <div className="flex gap-5">
-        <label className="flex items-center gap-1">
-          <input type="checkbox" />
-          Characters
-        </label>
-        <label className="flex items-center gap-1">
-          <input type="checkbox" />
-          Characters
-        </label>
-        <label className="flex items-center gap-1">
-          <input type="checkbox" />
-          Characters
-        </label>
-      </div>
+    <div className="flex items-center justify-center gap-24 border-b-2 border-b-white py-3">
+      <input
+        type="text"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        className="block w-80 rounded-xl px-4 py-2 text-gray-900 outline-none"
+      />
+      <Link
+        href={"characters/lol"}
+        className="rounded-lg bg-blue-700 px-4 py-2 text-white hover:bg-white hover:text-black"
+      >
+        Search
+      </Link>
+      <div className="flex gap-5">Results Found</div>
     </div>
   );
 }
